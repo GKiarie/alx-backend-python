6cf7ddda3cd953c45ac7dbabd64256b016b848ac
GKiarie <igitaukiarie@gmail.com>
2023-07-06 22:53:22 +0300

    function sum_list which takes a list input_list of floats as argument and returns their sum as a float

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ftn takes in list of floats, returns sum floats"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """takes in list returns sum"""
     return sum(input_list)
