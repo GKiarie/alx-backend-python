a7ae3af0107aa37361e2144a9af7006c35905951
GKiarie <igitaukiarie@gmail.com>
2023-07-10 21:23:57 +0300

    measuring runtime of an sync ftn

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Ftn to measure runtime"""
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Ftn to measure runtime"""
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    total_time = end_time - start_time
+    average_time = total_time / n
+    return average_time
