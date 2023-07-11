c97a552846190dabd57af4e5e42816882e5dbd76
GKiarie <igitaukiarie@gmail.com>
2023-07-11 20:54:25 +0300

    pycodestyle corr tyask 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,6 +8,7 @@ from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
+
 async def async_comprehension() -> List[float]:
     """Coroutine to collect 10 random numbers using async comprehension"""
     random_numbers = [number async for number in async_generator()]
